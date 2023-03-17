227defba3debd71eaab1377f9e6e47d35b9277c8
Little Bit <cthibodeau09@gmail.com>
2023-03-17 15:19:54 -0400

    made changes to JS, CSS, HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,10 @@
+$(document).ready(function () {
+
 var APIKEY = "d67149e00c3ad3b928d7c75156d20ed8"; 
 
 var city;
 
-var queryURL = https://api.openweathermap.org/data/2.5/weather?q={orlando}&appid={d67149e00c3ad3b928d7c75156d20ed8}
+var queryURL = https://api.openweathermap.org/data/2.5/weather?q={orlando}&appid={d67149e00c3ad3b928d7c75156d20ed8};
 
 fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&key=YOUR_API_KEY&contentType=json", {
   "method": "GET",
@@ -14,4 +16,6 @@ fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/service
 })
 .catch(err => {
   console.error(err);
+});
+
 });
